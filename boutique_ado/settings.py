7d6c8f1bffe8c372e6241ca30ba03dc81237f48e
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-01-26 19:31:39 +0000

    changed secret key to env file and changed debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vj0tq)xqsdl&^3x-qdeww7&kg(mqca4843z3u!yo@8k%@ou3as'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-leoniemclau-boutiqueado-n6d16mlmy34.ws-eu107.gitpod.io', 
